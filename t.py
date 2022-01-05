fd7b121f06f63979f5e43f7136b76ad5a4a0f33f
Joao Meyer <1994meyer@gmail.com>
2022-01-05 23:55:17 +0000

    Added first version

--- t.py
+++ t.py
@@ -6,7 +6,6 @@ from pydantic import conint
 from starlette.responses import StreamingResponse
 from playwright.async_api import async_playwright
 
-
 app = FastAPI()
 
 browser = None
@@ -27,7 +26,22 @@ async def shutdown():
 
 
 @dataclass
-class Req:
+class Click:
+    x: conint(gt=0, le=1920) = None
+    y: conint(gt=0, le=1920) = None
+
+    @property
+    def clickable(self) -> bool:
+        return None not in (self.x, self.y)
+
+    @property
+    def position(self) -> dict:
+        return {'x': self.x, 'y': self.y}
+
+
+@dataclass
+class Page:
+    id: str
     url: str
     width: conint(gt=100, le=1920) = 1920
     height: conint(gt=100, le=1920) = 1080
@@ -37,12 +51,30 @@ class Req:
         return {'width': self.width, 'height': self.height}
 
 
+@dataclass
+class Ctx:
+    id: str
+    page: Page = Depends(Page)
+    click: Click = Depends(Click)
+
+
+SESSIONS = {}
+
+
 @app.get('/')
-async def get(req: Req = Depends(Req)) -> StreamingResponse:
-    page = await browser.new_page()
-    await page.set_viewport_size(req.size)
-    await page.goto(req.url)
-    buff = await page.screenshot(full_page=True)
+async def get(ctx: Ctx = Depends(Ctx)):
+    if ctx.id in SESSIONS:
+        tab = SESSIONS[ctx.id]
+    else:
+        tab = await browser.new_page()
+        await tab.goto(ctx.page.url)
+        SESSIONS[ctx.id] = tab
+
+    await tab.set_viewport_size(ctx.page.size)
+
+    if ctx.click.clickable:
+        await tab.click('html', position=ctx.click.position)
+
+    buff = await tab.screenshot(type='jpeg', quality=50)
     buff = BytesIO(buff)
-    await page.close()
-    return StreamingResponse(buff, media_type='image/png')
+    return StreamingResponse(buff, media_type='image/jpeg')
